57e197ac48efdb3467b90b5e30bb0fea23159b61
Juan Baltazar <juan.baltazar1@gmail.com>
2020-12-23 21:45:01 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="main">
+        <header>
+          <nav>
+            <img className="logo" src={logo} alt="ct-logo"/>
+            <ul className="nav-items">
+              <li>
+                <a href="#history">
+                  01. HISTORY
+                </a>
+              </li>
+              <li>
+                <a href="#team">
+                  02. TEAM
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <h1>
+          <span className="LA">
+            LOS ANGELES
+          </span>
+          <span className="mount">
+            MOUNTAINS
+          </span>
+        </h1>
+        <footer>
+          <div className="foot-wrapper">
+
+          <div className="logo-and-la">
+            <img className="foot-logo" src={logo} alt="ct-logo"/>
+            <div className="la-mount">
+              <span className="foot-la">
+                LOS ANGELES
+              </span>
+              <span className="foot-mount">
+                MOUNTAINS
+              </span>
+            </div>
+          </div>
+            <ul className="foot-items">
+              <li>
+                <a href="#history">
+                  01. HISTORY
+                </a>
+              </li>
+              <li>
+                <a href="#team">
+                  02. TEAM
+                </a>
+              </li>
+            </ul>
+          </div>
+        </footer>
+      </main>
     </div>
   );
 }
