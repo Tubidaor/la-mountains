0e3a3a5c4af241f323bc5e08554d81ad4a1868bc
Juan Baltazar <juan.baltazar1@gmail.com>
2020-12-27 21:19:44 -0800

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import photo3 from './assets/photo3.jpg'
 import photo4 from './assets/photo4.jpg'
 import thirdPageFooter from './assets/3rdPageFooter.png'
 import climbHeading from './assets/climbHeading.png'
+import historyHeading from './assets/historyHeading.png'
+
 
 export default class App extends Component {
   
@@ -16,6 +18,33 @@ export default class App extends Component {
       pics: [photo1, photo2, photo3, photo4],
       index: null,
       mountId: "mount1"
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.stickNav)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.stickNav)
+  }
+
+  stickNav = () => {
+    const footer = document.getElementById("stick-con")
+    const stay = document.getElementById("stick")
+    const stick = footer.getBoundingClientRect()
+
+    if(stick.top <= 0) {
+      stay.style.position = "fixed"
+      stay.style.top = 0
+      stay.style.width = "100%"
+      stay.style.backgroundColor = "white"
+      stay.style.bottom = null;
+    }
+    if(stick.top >= 0) {
+      stay.style.position = "absolute"
+      stay.style.bottom = 0;
+      stay.style.width = "100%"
     }
   }
 
@@ -49,6 +78,7 @@ export default class App extends Component {
       index
     })
   }
+
   handleSchedClick = (mountId) => {
     const id = document.getElementById(mountId)
     const li = document.getElementsByClassName("m-choice")
@@ -69,12 +99,14 @@ export default class App extends Component {
     let close = document.getElementById('close')
     close.style.display = "none"
   }
+
   render() {
+
     return (
       <div className="App">
         <main className="main">
           <header>
-            <nav>
+            <nav className="main-nav">
               <img className="logo" src={logo} alt="ct-logo"/>
               <ul className="nav-items">
                 <li>
@@ -95,62 +127,100 @@ export default class App extends Component {
               LOS ANGELES MOUNTAINS
             </h1>
           </header>
-          <footer className="sticky-footer">
-            <div className="foot-wrapper">
-              <div className="logo-and-la">
-                <img className="foot-logo" src={logo} alt="ct-logo"/>
-                <div className="la-mount">
-                  <span className="foot-la">
-                    LOS ANGELES
-                  </span>
-                  <span className="foot-mount">
-                    MOUNTAINS
-                  </span>
+          <footer className="sticky-footer" id="stick-con">
+            <div  id="stick">
+              <div className="foot-wrapper">
+                <div className="logo-and-la">
+                  <img className="foot-logo" src={logo} alt="ct-logo"/>
+                  <div className="la-mount">
+                    <span className="foot-la">
+                      LOS ANGELES
+                    </span>
+                    <span className="foot-mount">
+                      MOUNTAINS
+                    </span>
+                  </div>
                 </div>
+                <ul className="foot-items">
+                  <li>
+                    <a href="#history">
+                      01. HISTORY
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#team">
+                      02. TEAM
+                    </a>
+                  </li>
+                </ul>
               </div>
-              <ul className="foot-items">
-                <li>
-                  <a href="#history">
-                    01. HISTORY
-                  </a>
-                </li>
-                <li>
-                  <a href="#team">
-                    02. TEAM
-                  </a>
-                </li>
-              </ul>
             </div>
           </footer>
         </main>
         <section className="history" id="history">
-          <h2 className="history-h2">
-            01. HISTORY
-          </h2>
+          <header className="history-header">
+            <h2 className="history-h2">
+              01. HISTORY
+            </h2>
+            <img className="history-heading-img" src={historyHeading}></img>
+          </header>
           <p className="history-intro">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Proin in ante viverra, rutrum erat rutrum, consectetur mi.
             Proin at maximus est. Nullam purus ex, iaculis sed erat sed,
             blandit tincidunt quam. Cras scelerisque id quam sed dignissim
-            Pellentesque urna nunc, gravida quis hendrerit ac, tristique ut quam.
-            Vivamus suscipit dignissim tortor nec congue. 
+            Pellentesque urna nunc, gravida quis hendrerit ac, tristique
+            ut quam. Vivamus suscipit dignissim tortor nec congue. 
           </p>
-          <div className="main-pic-con hide-pic" id="close" onClick={e => this.handleClose()}>
+          <div 
+            className="main-pic-con hide-pic"
+            id="close"
+            onClick={e => this.handleClose()}
+          >
             <div className="close-pic">
               <div className="bar bar1">
               </div>
               <div className="bar bar2">
               </div>
             </div>
-
-            {this.state.index != null && <img className="main-pic" src={this.state.pics[this.state.index]}></img>}
+            {
+              this.state.index != null &&
+              <img 
+                className="main-pic"
+                src={this.state.pics[this.state.index]}
+              >
+              </img>
+            }
           </div>
           <div className="carousel">
             <div className="pics-wrapper">
-              <img className="side-pic" id="img1" src={photo1} alt="Man on top of mountain." onClick={e => this.handlePicClick(0, "img1", "bubble1")}/>
-              <img className="side-pic" id="img2" src={photo2} alt="Man on top of mountain." onClick={e => this.handlePicClick(1, "img2", "bubble2")}/>
-              <img className="side-pic" id="img3" src={photo3} alt="Man on top of mountain." onClick={e => this.handlePicClick(2, "img3", "bubble3")}/>
-              <img className="side-pic" id="img4" src={photo4} alt="Man on top of mountain." onClick={e => this.handlePicClick(3, "img4", "bubble4")}/>
+              <img
+                className="side-pic"
+                id="img1" src={photo1}
+                alt="Man on top of mountain."
+                onClick={e => this.handlePicClick(0, "img1", "bubble1")}
+              />
+              <img
+                className="side-pic"
+                id="img2"
+                src={photo2}
+                alt="Man on top of mountain."
+                onClick={e => this.handlePicClick(1, "img2", "bubble2")}
+              />
+              <img
+                className="side-pic"
+                id="img3"
+                src={photo3}
+                alt="Man on top of mountain."
+                onClick={e => this.handlePicClick(2, "img3", "bubble3")}
+              />
+              <img
+                className="side-pic"
+                id="img4"
+                src={photo4}
+                alt="Man on top of mountain."
+                onClick={e => this.handlePicClick(3, "img4", "bubble4")}
+              />
             </div>
             <div className="bubble-selector-con">
               <div className="bubble" id="bubble1">
@@ -177,13 +247,21 @@ export default class App extends Component {
                 dignissim tortor nec congue.
               </p>
             </header>
-            <header>
+            <header className="team-nav-header">
               <nav className="team-nav">
                 <ul className="team-ul">
-                  <li className="m-choice" id="mount1" onClick={e => this.handleSchedClick("mount1")}>
+                  <li
+                    className="m-choice mount-selected"
+                    id="mount1"
+                    onClick={e => this.handleSchedClick("mount1")}
+                  >
                     MOUNTAIN 1
                   </li>
-                  <li className="m-choice" id="mount2" onClick={e => this.handleSchedClick("mount2")}>
+                  <li
+                    className="m-choice"
+                    id="mount2"
+                    onClick={e => this.handleSchedClick("mount2")}
+                  >
                     MOUNTAIN 2
                   </li>
                 </ul>
@@ -296,12 +374,18 @@ export default class App extends Component {
                 </div>
               </div>
           <footer className="sched-footer">
-            <img className="sched-foot-img" src={thirdPageFooter} alt="LA mountains footer image."/>
-            <span className="copyright">COPYRIGHT 2016. ALL RIGHTS RESERVED.</span>
+            <img
+              className="sched-foot-img"
+              src={thirdPageFooter}
+              alt="LA mountains footer image."
+            />
+            <span className="copyright">
+              COPYRIGHT 2016. ALL RIGHTS RESERVED.
+            </span>
           </footer>
         </section>
       </div>
-    );
+    )
   }
 }
 
